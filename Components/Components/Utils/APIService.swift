4ce6e9ca50b75410571c9000974ba710ee7f0de0
Kevin Maulana <kimlaoxing@gmail.com>
2022-10-31 09:37:15 +0700

    feat components

---
+++ Components/Components/Utils/APIService.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public struct APIService {
+    public static let apiKey = "059f693249143cb6f695327782396d54"
+    public static let basePath = "https://api.themoviedb.org/3"
+    public static let detailBasePath = "https://api.themoviedb.org/3/movie"
+    public static let basePathImage = "https://image.tmdb.org/t/p/w500"
+}
