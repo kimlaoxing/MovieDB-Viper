5264d92de6eb3f1043bbd9b4eb3fe893ac16d0c2
Kevin Maulana <kimlaoxing@gmail.com>
2022-10-31 09:22:09 +0700

    Initial Commit

---
+++ MovieDB-Viper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB-Viper
+//
+//  Created by Kevin Maulana on 31/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
